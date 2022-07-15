5059d96ac4a1811d6e1a07eab4cdb7f7f7b8069a
wappon_28_dev <wmachi0409@gmail.com>
2022-07-15 16:13:51 +0900

    updated_build

---
+++ src/index.svelte
@@ -0,0 +1,10 @@
+<script>
+  import Home from "./Home.svelte";
+  import Recruit from "./recruit.svelte";
+</script>
+
+<svelte:head>
+  <title>Welcome</title>
+</svelte:head>
+
+<Home />
--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,14 @@ export default {
   preprocess: sveltePreprocess(),
 
   kit: {
-    adapter: adapter({ out: 'public ' }),
+    adapter: adapter({
+      // default options are shown
+      pages: "public",
+      assets: "public",
+      fallback: 'index.html',
+      precompress: true,
+    }),
+
     prerender: {
       // This can be false if you're using a fallback (i.e. SPA mode)
       default: true,
