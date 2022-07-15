7a21cc9cfbc61de0a79e226b2e02ee6002c55348
wappon_28_dev <wmachi0409@gmail.com>
2022-07-15 15:45:48 +0900

    updated_build_properties

--- svelte.config.js
+++ svelte.config.js
@@ -5,14 +5,7 @@ export default {
   preprocess: sveltePreprocess(),
 
   kit: {
-    adapter: adapter({
-      // default options are shown
-      pages: "public",
-      assets: "public",
-      fallback: null,
-      precompress: false,
-    }),
-
+    adapter: adapter({ out: 'public ' }),
     prerender: {
       // This can be false if you're using a fallback (i.e. SPA mode)
       default: true,
